de24ad656588269596282f5de22bb22e69945570
Demola <90010323+trustbycode@users.noreply.github.com>
2022-09-14 16:28:04 +0100

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,1 @@
-#include "main.h"
-/**
- * main - Entry point
- *
- * Return: ALways 0 (Success)
- */
-int main(void)
-{
-	char c[] = "_putchar";
-	int i = 0;
 
-	while (i < 8)
-	{
-		_putchar(c[i]);
-		i++;
-	}
-	_putchar('\n');
-	return (0);
-}
