0d74dce68ada1ae03e2a9d44c9cf71c5a1485477
Demola <90010323+trustbycode@users.noreply.github.com>
2022-09-14 16:59:42 +0100

    "Print Alphabet C"

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet ten times
+ * *
+ * Return: 0 (success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 97; b <= 122; b++)
+		{
+			_putchar(b);
+		}
+		_putchar('\n');
+	}
+}
