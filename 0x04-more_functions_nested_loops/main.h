5d4230a0655f72e3616c1607cef10fb98245e06a
Demola <90010323+trustbycode@users.noreply.github.com>
2022-09-16 08:56:52 +0100

    main h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,13 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
 int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int n);
-int print_last_digit(int);
-void jack_bauer(void);
-int add(int, int);
-void print_times_table(int n);
-void times_table(void);
-void print_to_98(int n);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void num_to_char(unsigned int);
+void print_number(int n);
+
+#endif /* MAIN_H */
