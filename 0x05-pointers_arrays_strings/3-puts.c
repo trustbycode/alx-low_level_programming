f637fb26e0d66cef941594d2e5816bf4d02f1327
Demola <90010323+trustbycode@users.noreply.github.com>
2022-09-19 22:36:21 +0100

    3 puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,15 +1,20 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	char *str;
+* _puts - prints a string follwed by a new line, to stdout
+* @str: checked
+* Return: nothing
+*/
 
-	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-	 _puts(str);
-	return (0);
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		putchar(*(str + i));
+		i++;
+	}
+	putchar(10);
 }
