05e1fb28cd54ec58257327de9c899d840ab479b1
Demola <90010323+trustbycode@users.noreply.github.com>
2022-09-19 22:50:43 +0100

    6 puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* puts2 - prints every character of a string followed by a new line
+* @str: string parameter
+* Return: Always 0
+*/
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0; ++i)
+	{
+		if (i % 2==0);
+			putchar(str[i]);
+	}
+	putchar('\n');
+}
