d85156575e7216fe4037c375f2e92cf8dc85aca2
Demola <90010323+trustbycode@users.noreply.github.com>
2022-09-19 23:09:51 +0100

    4 print

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,15 +8,14 @@
 
 void print_rev(char *s)
 {
-	int i = 0;
+int i = 0;
+while (s[i])
+	i++;
 
-	while (*(s + i) != '\0')
-		i += 1;
-	i -= 1;
-	while (i >= 0)
-	{
-		_putchar(*(s + i));
-		i--;
-	}
-	_putchar('\n');
+while (i--)
+{
+	_putchar(s[i]);
+}
+_putchar('\n');
+
 }
