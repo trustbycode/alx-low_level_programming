5a3c3340aedfec4c130236efe0cf17541e6aaea9
Demola <90010323+trustbycode@users.noreply.github.com>
2022-09-19 22:38:29 +0100

    4print

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_rev - prints a string, in reverse, followed by a new line
+* @s: string to reverse
+* Return: nothing
+*/
+
+void print_rev(char *s)
+{
+	if (*str)
+	{
+		while (*str)
+		{
+			putchar(*str++);
+		}
+	}
+	_putchar('\n);
+}
