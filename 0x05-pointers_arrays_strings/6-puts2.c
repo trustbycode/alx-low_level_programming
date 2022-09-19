6fc79863ac7a04ec3fb44f941b9929807e754b02
Demola <90010323+trustbycode@users.noreply.github.com>
2022-09-19 23:14:52 +0100

    put 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,9 +1,10 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * puts2 - function that prints every other character
+ * puts2 - prints every other character
+ * @str: string
  *
- * @str: pointer to char
  * Return: nothing
  */
 
@@ -11,13 +12,11 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (*(str + i) != '\0')
 	{
 		if (i % 2 == 0)
-		{
-			_putchar(*(str + i));
-		}
-		i += 1;
+			putchar(*(str + i));
+		i++;
 	}
-	_putchar('\n');
+	putchar(10);
 }
