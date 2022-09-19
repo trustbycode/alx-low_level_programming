593990f90f0f393d76e5e820cc015c68003a3709
Demola <90010323+trustbycode@users.noreply.github.com>
2022-09-19 23:13:11 +0100

    print 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,21 +1,18 @@
+#include <stdio.h>
+#include <string.h>
 #include "main.h"
 
 /**
-* print_rev - prints a string, in reverse, followed by a new line
-* @s: string to reverse
-* Return: nothing
+*print_rev - Prints a string in reverse order
+*@s: String to reverse
+*Return: Nothing
 */
 
 void print_rev(char *s)
 {
-int i = 0;
-while (s[i])
-	i++;
+	int len = strlen(s);
 
-while (i--)
-{
-	_putchar(s[i]);
-}
-_putchar('\n');
-
+	while (len--)
+		putchar(*(s + len));
+	putchar(10);
 }
