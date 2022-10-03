d95c64908e7892925fab9d1b2695fa7371271ea4
Demola <90010323+trustbycode@users.noreply.github.com>
2022-10-04 00:41:14 +0100

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,25 +6,25 @@
  * create_array - Creates an array for the function.
  * @size: is the size of unsigned int array
  * @c: is a character for char.
- * 
+ *
  * Return: Returns to the array.
- */
+**/
 
 char *create_array(unsigned int size, char c)
 {
-	char *array = NULL;
-	unsigned int i;
+char *array = NULL;
+unsigned int i;
 
-	if (size == 0)
-		return (NULL);
-	if (size != 0)
-	{
-		array = (char *)malloc(size * sizeof(char));
-		if (array != NULL)
-		{
-			for (i = 0; i < size; i++)
-				array[i] = c;
-		}
-	}
-	return (array);
+if (size == 0)
+return (NULL);
+if (size != 0)
+{
+array = (char *)malloc(size * sizeof(char));
+if (array != NULL)
+{
+for (i = 0; i < size; i++)
+array[i] = c;
+}
+}
+return (array);
 }
